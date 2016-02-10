dd2a3eee4168a70bddc122ab1274f5800a25115e
Daniel Nilsson <danielenilsson@gmail.com>
2016-02-10 21:27:37 +0100

    Move from userena to custom

---
+++ accounts/forms.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from userena.forms import SignupFormOnlyEmail
+from userena import settings as userena_settings
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.utils.translation import ugettext_lazy as _
+import random
+
+class CustomSignupForm(SignupFormOnlyEmail):
+	"""custom SignupForm"""
+	def save(self):
+		new_user = super(CustomSignupForm, self).save()
+		if new_user.username.isdigit():
+			new_user = CustomSignupForm.regenerate_username(new_user)
+			new_user.save()
+		return new_user
+
+	@classmethod
+	def regenerate_username(cls, new_user):
+		""" Generate a random username that also contains letters, not only digits. """
+		is_username_taken = False
+		while new_user.username.isdigit() or is_username_taken:
+			username = sha_constructor(str(random.random())).hexdigest()[:5]
+			new_user.username = username
+			is_username_taken = User.objects.filter(username__iexact=username).exists()
+		return new_user
+
+class AuthenticationForm(forms.Form):
+	identification = forms.CharField(
+		widget=forms.TextInput(attrs={'class': 'required form-control',
+								'placeholder': _(u"Enter your e-mail address")}),
+		label=_(u"E-mail address"),
+		max_length=75,
+		error_messages={'required': _(u"Please supply your email.")}
+    )
+	password = forms.CharField(
+		widget=forms.PasswordInput(attrs={'class': 'required form-control',
+											'placeholder': _(u"Enter your password")},
+											render_value=False),
+		label=_("Password"),
+	)
+	remember_me = forms.BooleanField(
+		widget=forms.CheckboxInput(),
+		label=_(u'Remember me for %(days)s') % {'days': _(userena_settings.USERENA_REMEMBER_ME_DAYS[0])},
+		required=False
+	)
+
+	def clean(self):
+		"""
+		Checks for the identification and password.
+
+		If the combination can't be found will raise an invalid sign in error.
+		"""
+		identification = self.cleaned_data.get('identification')
+		password = self.cleaned_data.get('password')
+
+		if identification and password:
+			user = authenticate(identification=identification, password=password)
+			if user is None:
+				error_msg = _(u"Please enter a correct username or email and password. Note that both fields are case-sensitive.")
+				raise forms.ValidationError(error_msg)
+		return self.cleaned_data
+
+		
---
+++ accounts/urls.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+from accounts import views
+from roleplaying import settings
+
+urlpatterns = [
+	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^signin/$', views.signin, name="signin"),
+	url(r'^(?P<username>[\.\w-]+)/signup/complete/$',
+        views.signup_complete,
+        name='signup_complete'),
+    url(r'^password/reset/$',
+    auth_views.password_reset,
+    {'template_name': 'password_reset_form.html',
+     'email_template_name': 'emails/password_reset_message.txt',
+     'extra_context': {'without_usernames': settings.USERENA_WITHOUT_USERNAMES}
+    },
+    name='password_reset'),
+    url(r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        {'template_name': 'password_reset_done.html'},
+        name='password_reset_done'),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+?)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        {'template_name': 'password_reset_confirm_form.html'},
+        name='password_reset_confirm'),
+    url(r'^password/reset/confirm/complete/$',
+        auth_views.password_reset_complete,
+        {'template_name': 'password_reset_complete.html'},
+        name='password_reset_complete'),
+    url(r'^(?P<username>[\.\w-]+)/' views.profile, name="user_profile"),
+    url(r'^(?P<username>[\.\w-]+)/edit/' views.edit_profile, name="edit_profile"),
+]
