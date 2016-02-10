9d2bdb0203b3f23ea6c734fd4398b5faa7c41da3
svamp <danielenilsson@gmail.com>
2016-02-10 13:40:40 +0100

    Set static and media dirs updates requierments

--- roleplaying/settings.py
+++ roleplaying/settings.py
@@ -149,4 +149,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = HOME_URL + '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_DIR, '..', 'static'))
+
+MEDIA_URL = HOME_URL + '/media/'
+MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_DIR, '..', 'media'))
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+APPEND_SLASH = True
