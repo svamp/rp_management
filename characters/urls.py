aba66d4049e99fc1790083ee5834dc6a147e046b
Daniel Nilsson <danielenilsson@gmail.com>
2016-02-11 15:02:07 +0100

    Adds a view to remove characters

--- characters/urls.py
+++ characters/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import include, url
 from characters import views
 
 urlpatterns = [
-	url(r'^create/(?P<form_type>[-\w]+)/$', views.create_view, name='create_character'),
+	url(r'^create/(?P<form_type>[-\w]+)/$', views.create_view, name='create_form'),
+	url(r'^remove/character/(?P<character_id>\d{1,19})/$', views.remove_character, name='remove_character')
 ]
--- characters/views.py
+++ characters/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
 
 from characters.models import Weapons, Armor, Items, Race, RaceOrigin, Religion, Background, Archetype, Characteristic, \
 						Skills, SkillImprovement, ExceptionalCharacteristic, CharacteristicDetail, Spells, Character, \
@@ -10,26 +11,27 @@ from characters.models import Weapons, Armor, Items, Race, RaceOrigin, Religion,
 
 from characters.forms import *
 
+FORM_LIST = {
+	'weapon': CreateWeaponsForm(),
+	'armor': CreateArmorForm(),
+	'items': CreateItemForm(),
+	'race': CreateRaceForm(),
+	'race_origin': CreateRaceOriginForm(),
+	'religion': CreateReligionForm(),
+	'background': CreateBackgroundForm(),
+	'archetype': CreateArchetypeForm(),
+	'characteristic': CreateCharacteristicForm(),
+	'skill': CreateSkillsForm(),
+	'skill_improvement': CreateSkillImprovementForm(),
+	'exceptional_characteristic': CreateExceptionalCharacteristicForm(),
+	'characteristic_detail': CreateCharacteristicDetailForm(),
+	'spells': CreateSpellsForm(),
+	'character': CreateCharacterForm(),
+}
 
 def create_view(request, form_type):
-	form_list = {
-		'weapon': CreateWeaponsForm(),
-		'armor': CreateArmorForm(),
-		'items': CreateItemForm(),
-		'race': CreateRaceForm(),
-		'race_origin': CreateRaceOriginForm(),
-		'religion': CreateReligionForm(),
-		'background': CreateBackgroundForm(),
-		'archetype': CreateArchetypeForm(),
-		'characteristic': CreateCharacteristicForm(),
-		'skill': CreateSkillsForm(),
-		'skill_improvement': CreateSkillImprovementForm(),
-		'exceptional_characteristic': CreateExceptionalCharacteristicForm(),
-		'characteristic_detail': CreateCharacteristicDetailForm(),
-		'spells': CreateSpellsForm(),
-		'character': CreateCharacterForm(),
-	}
-	form = form_list[form_type]
+
+	form = FORM_LIST[form_type]
 
 	if request.method == "POST":
 
@@ -47,3 +49,16 @@ def create_view(request, form_type):
 
 	return render(request, 'create.html', args)
 
+def save_item(request, form_type):
+	form = FORM_LIST[form_type]
+	pass
+
+def remove_character(request, character_id):
+	try:
+		character = get_object_or_404(Character, pk=character_id, creator=request.user)
+		character.delete()
+	except Exception:
+		pass
+
+	url = reverse('password_change', args=[request.user.username])
+	return HttpResponseRedirect(url)
