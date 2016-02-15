01b3cd66d6b39d72e91a03b4a84ff16211c2bd79
Daniel Nilsson <danielenilsson@gmail.com>
2016-02-15 23:10:53 +0100

    Added new styling to the admin interface

--- roleplaying/settings.py
+++ roleplaying/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.auth',
